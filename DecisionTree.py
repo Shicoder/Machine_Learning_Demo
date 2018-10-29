83059e20f14a668e76dff2697badca4a8393c422
shixiangfu <shixiangfu@youzan.com>
2018-10-29 11:36:59 +0800

    tree

---
+++ DecisionTree.py
@@ -0,0 +1,31 @@
+# -*- coding:utf-8 -*-
+'''
+__author__ = 'xfu_shi'
+'''
+
+
+import operator
+from numpy import *
+from math import log
+
+class DecisionTree:
+    def __init__(self):
+        return None
+
+    def entropy(self,y):
+        return None
+    def fit(self):
+        return None
+    def predict(self):
+        return None
+
+
+
+
+if __name__ == '__main__':
+    x = []
+    y = []
+
+    clf = None
+    clf.fit()
+    clf.predixt()
